06f3492199ef8bf2dd9a3e21efdf337b348e11a5
MWladislav <matujzo_vv_16@mf.grsu.by>
2019-05-07 01:32:49 +0300

    Initial commit

---
+++ src/main/java/by/grsu/coursework/fingerprinting/FingerprintingApplication.java
@@ -0,0 +1,13 @@
+package by.grsu.coursework.fingerprinting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingerprintingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FingerprintingApplication.class, args);
+    }
+
+}
---
+++ src/main/java/by/grsu/coursework/fingerprinting/controller/HomeController.java
@@ -0,0 +1,8 @@
+package by.grsu.coursework.fingerprinting.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+}
